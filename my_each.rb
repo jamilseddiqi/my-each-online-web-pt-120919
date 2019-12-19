fd2fbe783a4e131ed7fea223cb4c1bb81e0f9e33
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-19 13:07:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,10 @@
     def my_each(array)
       i = 0
       while i < array.length do
-      array.collect << array.new
+      array.collect
     # The "while" method will always return a value of => nil"
         yield(array[i])
         i = i + 1
       end
+      array 
     end
